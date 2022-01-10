53984a94ad8534783bc4c187918e344cb1c0af6a
Romain <romain.theodet@hotmail.fr>
2022-01-10 20:07:42 +0100

    feat(view): History component
    WIP, skeleton only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 
 import {AppComponent} from "./app.component";
+import {HistoryComponent} from "./history/history.component";
 import {ShellComponent} from "./shell/shell.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         ShellComponent,
+        HistoryComponent,
     ],
     imports: [
         BrowserModule,
