b99275098d54b4c5d39fbfac4c5ea89ce7824646
Romain <romain.theodet@hotmail.fr>
2022-01-11 18:02:16 +0100

    refactor: Base TypeScript files src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
-import {PromptComponent} from "src/app/prompt/prompt.component";
 
-import {AppComponent} from "./app.component";
-import {HistoryComponent} from "./history/history.component";
+import {AppComponent} from "app.component";
+import {HistoryComponent} from "history/history.component";
+import {PromptComponent} from "prompt/prompt.component";
 
 @NgModule({
     declarations: [
