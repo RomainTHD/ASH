a56600177f37638b7bb3a48037adfd9454929a1a
Romain <romain.theodet@hotmail.fr>
2022-01-16 15:04:38 +0100

    feat: `mkdir`

---
+++ src/app/cmd/mkdir.spec.ts
@@ -0,0 +1,7 @@
+import {Mkdir} from "./mkdir";
+
+describe("Mkdir", () => {
+    it("should create an instance", () => {
+        expect(new Mkdir()).toBeTruthy();
+    });
+});
---
+++ src/app/cmd/mkdir.ts
@@ -0,0 +1,29 @@
+import {Command} from "app/cmd/command";
+import {Env} from "app/env";
+import {Directory} from "app/fs";
+
+export class Mkdir extends Command {
+    public override readonly description = "Create a directory";
+    public override readonly usage       = "mkdir <path>";
+
+    public override async run(args: string[], env: Env): Promise<string> {
+        let path    = args[0];
+        let pathArr = path.split("/");
+        const name  = pathArr.pop() as string;
+        path        = pathArr.join("/");
+
+        const dir = Directory.findFromPath(path);
+        if (!dir) {
+            return "No such directory";
+        }
+
+        Directory.create({
+            name,
+            parent: dir.id,
+            owner: "",
+            content: [],
+        });
+
+        return "Created";
+    }
+}
