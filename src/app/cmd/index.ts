c79e8151e96be8f0534885bb3fadc9b15a69f490
Romain <romain.theodet@hotmail.fr>
2022-01-16 15:06:58 +0100

    feat: new commands
    See previous commits

--- src/app/cmd/index.ts
+++ src/app/cmd/index.ts
@@ -1,5 +1,25 @@
+import {Cat} from "./cat";
+import {Cd} from "./cd";
+// import {Cp} from "./cp";
 import {Command} from "./command";
+import {Echo} from "./echo";
+// import {Exit} from "./exit";
+// import {Grep} from "./grep";
+// import {Help} from "./help";
 import {Ls} from "./ls";
+import {Mkdir} from "./mkdir";
+// import {Mv} from "./mv";
+import {Pwd} from "./pwd";
+import {Touch} from "./touch";
 
 export {Command};
-export {Ls};
+
+export {
+    Cat,
+    Cd,
+    Mkdir,
+    Echo,
+    Ls,
+    Pwd,
+    Touch,
+};
