76c1d4165c151e1250f348a4539b0320e27daad5
Romain <romain.theodet@hotmail.fr>
2022-01-18 11:16:47 +0100

    refactor: Exit codes

---
+++ src/app/process/exit-code.ts
@@ -0,0 +1,11 @@
+export enum ExitCode {
+    Success      = 0,
+    Error        = 1,
+    Cancelled    = 2,
+    Timeout      = 3,
+    Unknown      = 4,
+    Unsupported  = 5,
+    Unauthorized = 6,
+    NotFound     = 7,
+    Invalid      = 8,
+}
