9e17f879b13935b6bc4367695a4a8f7fa4a0b909
RomainTHD <romain-thd+github@protonmail.com>
2022-01-26 19:51:32 +0100

    test: ANSI colors

--- src/app/output/ansi-color.spec.ts
+++ src/app/output/ansi-color.spec.ts
@@ -1,9 +1,23 @@
 import {AnsiColor} from "app/output/ansi-color";
+import {front} from "app/utils";
 
 describe("AnsiColor", () => {
+    const msg = "Hello world";
+
     it("should keep the original text", () => {
-        expect(AnsiColor.parse("test")).toEqual("test");
-        expect(AnsiColor.parse(`${AnsiColor.RESET}test`)).toEqual("test");
-        expect(AnsiColor.parse(`test${AnsiColor.RESET}`)).toEqual("test");
+        expect(AnsiColor.parse(msg)).toEqual(msg);
+        expect(AnsiColor.parse(`${AnsiColor.RESET}${msg}`)).toEqual(msg);
+        expect(AnsiColor.parse(`${msg}${AnsiColor.RESET}`)).toEqual(msg);
+    });
+
+    it("should write the right HTML tag", () => {
+        const msg = "test";
+        const txt = AnsiColor.parse(`${AnsiColor.FG.BLUE}${msg}${AnsiColor.RESET}`);
+        const elt = front.stringToHTML(txt, true);
+        expect(elt).toBeInstanceOf(HTMLSpanElement);
+        expect(elt.classList).toContain("fg--blue");
+        // FIXME: Not the best way to test the color,
+        //  we should instead test the CSS
+        expect(elt.innerText).toEqual(msg);
     });
 });
